168072488257c40797911e0444c806e68055bcc6
Song, Xiao Ping <xp.song@u.nus.edu>
2022-02-19 01:00:39 +0800

    edit lsm_perpoint() to accept data without info for 'period'

--- R/lsm_perpoint.R
+++ R/lsm_perpoint.R
@@ -54,7 +54,7 @@ lsm_perpoint <- function(raster, points, buffer_sizes,
   checkmate::assert_numeric(buffer_sizes, lower = 0, finite = TRUE, any.missing = FALSE, add = coll)
   checkmate::assert_character(landscape_name, min.chars = 1, any.missing = FALSE, all.missing = FALSE, null.ok = TRUE, len = 1, add = coll)
   checkmate::assert_subset(point_id, choices = colnames(points), empty.ok = FALSE, add = coll)
-  checkmate::assert_subset(period, choices = colnames(points), empty.ok = FALSE, add = coll)
+  # checkmate::assert_subset(period, choices = colnames(points), empty.ok = FALSE, add = coll)
   checkmate::assert_subset(level, choices = c("class", "landscape"), empty.ok = TRUE, add = coll)
 
   checkmate::reportAssertions(coll)
@@ -65,8 +65,10 @@ lsm_perpoint <- function(raster, points, buffer_sizes,
     stop("All elements in buffer_size must be larger than the spatial (pixel) resolution of the input raster")
   }
 
-  if((terra::nlyr(raster) != length(unique(points[[period]]))) & any(points[[period]]%%1!=0)){
-    stop("The number of layers in 'raster' should correspond to the unique number of survey periods (integers) present in 'points'.")
+  if(period %in% colnames(points)){
+    if((terra::nlyr(raster) != length(unique(points[[period]]))) & any(points[[period]]%%1!=0)){
+      stop("The number of layers in 'raster' should correspond to the unique number of survey periods (integers) present in 'points'.")
+    }
   }
 
 
@@ -81,59 +83,118 @@ lsm_perpoint <- function(raster, points, buffer_sizes,
   circles <- list()
   for(i in 1:length(buffer_sizes)){
 
-     lsm <- landscapemetrics::sample_lsm(raster, points,
-                                         plot_id = paste0(points[[point_id]], "_period", points[[period]]), # combine point_id & period
-                                         shape = "circle",
-                                         size = buffer_sizes[i],
-                                         all_classes = TRUE, # NA for classes not present in some sample plots
+    if(period %in% colnames(points)){
+       lsm <- landscapemetrics::sample_lsm(raster, points,
+                                           plot_id = paste0(points[[point_id]], "_period", points[[period]]), # combine point_id & period
+                                           shape = "circle",
+                                           size = buffer_sizes[i],
+                                           all_classes = TRUE, # NA for classes not present in some sample plots
 
-                                         # arguments passed to calculate_lsm()
-                                         what = what,
-                                         level = level,
-                                         ...
-                                         ) %>%
+                                           # arguments passed to calculate_lsm()
+                                           what = what,
+                                           level = level,
+                                           ...
+                                           ) %>%
 
-       tidyr::separate(.data$plot_id, c(point_id, period), sep = "_period") %>% # separate cols
+         tidyr::separate(.data$plot_id, c(point_id, period), sep = "_period") %>% # separate cols
 
-       dplyr::filter(.data$layer == .data[[period]]) %>% # remove results for irrelevant period
-       dplyr::filter(!(level == "class" & class == 0) & !(level == "class" & is.na(class))) %>% # remove land cover class 0 or NA
+         dplyr::filter(.data$layer == .data[[period]]) %>% # remove results for irrelevant period
+         dplyr::filter(!(level == "class" & class == 0) & !(level == "class" & is.na(class))) %>% # remove land cover class 0 or NA
 
-       # remove useless metrics
-       dplyr::filter(!.data$metric %in% c("pr", "prd", "rpr")) %>%
-       dplyr::filter(!((.data$metric %in% c("ta")) & (level == "landscape"))) %>% # total landscape area
-       dplyr::filter(!((.data$metric %in% c("lpi")) & (level == "landscape"))) %>% # largest patch in landscape
+         # remove useless metrics
+         dplyr::filter(!.data$metric %in% c("pr", "prd", "rpr")) %>%
+         dplyr::filter(!((.data$metric %in% c("ta")) & (level == "landscape"))) %>% # total landscape area
+         dplyr::filter(!((.data$metric %in% c("lpi")) & (level == "landscape"))) %>% # largest patch in landscape
 
-       # new col for levels
-       dplyr::mutate(levels = dplyr::case_when(!is.na(class) ~ tibble::deframe(tibble(class_values, class_names))[class], # class-lvl
-                                               is.na(class) ~ paste0(landscape_name, "_l"))) %>% # landscape-lvl (NULL if not provided!)
+         # new col for levels
+         dplyr::mutate(levels = dplyr::case_when(!is.na(class) ~ tibble::deframe(tibble(class_values, class_names))[class], # class-lvl
+                                                 is.na(class) ~ paste0(landscape_name, "_l"))) %>% # landscape-lvl (NULL if not provided!)
 
-       tidyr::drop_na(levels) %>% # remove NA for classes not specified in arguments but present in raster
+         tidyr::drop_na(levels) %>% # remove NA for classes not specified in arguments but present in raster
 
-       # remove duplicated metrics at landscape lvl
-       dplyr::filter(!duplicated(cbind(.data[[point_id]], .data[[period]], .data$metric, .data$value))) %>%
+         # remove duplicated metrics at landscape lvl
+         dplyr::filter(!duplicated(cbind(.data[[point_id]], .data[[period]], .data$metric, .data$value))) %>%
 
 
 
-       tidyr::pivot_wider(id_cols = c(.data[[period]], .data[[point_id]], "percentage_inside"),
-                          names_from = c("levels", "metric"), values_from = "value",
-                          names_glue = "lsm_{levels}_{metric}") %>%
+         tidyr::pivot_wider(id_cols = c(.data[[period]], .data[[point_id]], "percentage_inside"),
+                            names_from = c("levels", "metric"), values_from = "value",
+                            names_glue = "lsm_{levels}_{metric}") %>%
 
-       # change specific cols (metrics) from NA to 0 (ONLY if percentage_inside > 90)
-       dplyr::mutate(dplyr::across(.cols = dplyr::ends_with("_area_mn") | # area (mean)
-                       dplyr::ends_with("ca") | # total class area
-                       dplyr::ends_with("_pland") | # class area % landscape
+         # change specific cols (metrics) from NA to 0 (ONLY if percentage_inside > 90)
+         dplyr::mutate(dplyr::across(.cols = dplyr::ends_with("_area_mn") | # area (mean)
+                         dplyr::ends_with("ca") | # total class area
+                         dplyr::ends_with("_pland") | # class area % landscape
 
-                       dplyr::ends_with("_core_mn") | # core area (mean)
-                       dplyr::ends_with("_tca") | # total core area
-                       dplyr::ends_with("_dcore_mn") | # disjunct core area (mean)
-                       dplyr::ends_with("_cpland") | # core area % landscape
+                         dplyr::ends_with("_core_mn") | # core area (mean)
+                         dplyr::ends_with("_tca") | # total core area
+                         dplyr::ends_with("_dcore_mn") | # disjunct core area (mean)
+                         dplyr::ends_with("_cpland") | # core area % landscape
 
-                       dplyr::ends_with("_te") | # total edge
-                       dplyr::ends_with("_ed") | # edge density
+                         dplyr::ends_with("_te") | # total edge
+                         dplyr::ends_with("_ed") | # edge density
 
-                       dplyr::ends_with("_np") | # no of patches
-                       dplyr::ends_with("_pd"), # patch density
-                     ~ifelse(is.na(.), 0, .)))
+                         dplyr::ends_with("_np") | # no of patches
+                         dplyr::ends_with("_pd"), # patch density
+                       ~ifelse(is.na(.), 0, .)))
+
+
+    # if period col not present in points
+    } else {
+
+      lsm <- landscapemetrics::sample_lsm(raster, points,
+                                          plot_id = points[[point_id]],
+                                          shape = "circle",
+                                          size = buffer_sizes[i],
+                                          all_classes = TRUE, # NA for classes not present in some sample plots
+
+                                          # arguments passed to calculate_lsm()
+                                          what = what,
+                                          level = level,
+                                          ...) %>%
+
+        dplyr::rename(point_id =.data$plot_id) %>% # separate cols
+        dplyr::filter(!(level == "class" & class == 0) & !(level == "class" & is.na(class))) %>% # remove land cover class 0 or NA
+
+        # remove useless metrics
+        dplyr::filter(!.data$metric %in% c("pr", "prd", "rpr")) %>%
+        dplyr::filter(!((.data$metric %in% c("ta")) & (level == "landscape"))) %>% # total landscape area
+        dplyr::filter(!((.data$metric %in% c("lpi")) & (level == "landscape"))) %>% # largest patch in landscape
+
+        # new col for levels
+        dplyr::mutate(levels = dplyr::case_when(!is.na(class) ~ tibble::deframe(tibble(class_values, class_names))[class], # class-lvl
+                                                is.na(class) ~ paste0(landscape_name, "_l"))) %>% # landscape-lvl (NULL if not provided!)
+
+        tidyr::drop_na(levels) %>% # remove NA for classes not specified in arguments but present in raster
+
+        # remove duplicated metrics at landscape lvl
+        dplyr::filter(!duplicated(cbind(.data[[point_id]], .data$metric, .data$value))) %>%
+
+
+
+        tidyr::pivot_wider(id_cols = c(.data[[point_id]], "percentage_inside"),
+                           names_from = c("levels", "metric"), values_from = "value",
+                           names_glue = "lsm_{levels}_{metric}") %>%
+
+        # change specific cols (metrics) from NA to 0 (ONLY if percentage_inside > 90)
+        dplyr::mutate(dplyr::across(.cols = dplyr::ends_with("_area_mn") | # area (mean)
+                                      dplyr::ends_with("ca") | # total class area
+                                      dplyr::ends_with("_pland") | # class area % landscape
+
+                                      dplyr::ends_with("_core_mn") | # core area (mean)
+                                      dplyr::ends_with("_tca") | # total core area
+                                      dplyr::ends_with("_dcore_mn") | # disjunct core area (mean)
+                                      dplyr::ends_with("_cpland") | # core area % landscape
+
+                                      dplyr::ends_with("_te") | # total edge
+                                      dplyr::ends_with("_ed") | # edge density
+
+                                      dplyr::ends_with("_np") | # no of patches
+                                      dplyr::ends_with("_pd"), # patch density
+                                    ~ifelse(is.na(.), 0, .)))
+
+
+    }
 
 
      message(paste0(Sys.time(), " Processed for ", i,"/", length(buffer_sizes),
